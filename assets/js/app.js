5e3d363814f10262dd1738853290b346bd31923b
gracemcken <grace.ellen.mckenna@gmail.com>
2022-09-14 16:18:22 +0100

    Fix js path in index, add tiles and keys in app.js

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+const tileDisplay = document.querySelector(".tile-container");
+const keyboard = document.querySelector(".key-container");
+
+const keys = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "ENTER",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+];
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key;
+    keyboard.append(buttonElement)
+})
