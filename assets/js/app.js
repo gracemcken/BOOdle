d64bbd8558a705a2f1e2a48c202017ae3d4304b8
gracemcken <grace.ellen.mckenna@gmail.com>
2022-09-17 17:55:59 +0100

    Add flipTile function with colors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 const tileDisplay = document.querySelector(".tile-container");
 const keyboard = document.querySelector(".key-container");
-const messageDisplay = document.querySelector('.message-container')
+const messageDisplay = document.querySelector(".message-container");
 
-const boodle = 'SPOOK'
+const boodle = "SPOOK";
 const keys = [
   "Q",
   "W",
@@ -31,101 +31,123 @@ const keys = [
   "B",
   "N",
   "M",
-  "<<"
+  "<<",
 ];
 
 const guessRows = [
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-]
-let currentRow = 0
-let currentTile = 0
-let isGameOver = false
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+let currentRow = 0;
+let currentTile = 0;
+let isGameOver = false;
 
 guessRows.forEach((guessRow, guessRowIndex) => {
-    const rowElement = document.createElement('div')
-    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
-    guessRow.forEach((guess, guessIndex) => {
-        const tileElement = document.createElement('div')
-        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
-        tileElement.classList.add('tile')
-        rowElement.append(tileElement)
-    })
-    tileDisplay.append(rowElement)
-})
+  const rowElement = document.createElement("div");
+  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement("div");
+    tileElement.setAttribute(
+      "id",
+      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+    );
+    tileElement.classList.add("tile");
+    rowElement.append(tileElement);
+  });
+  tileDisplay.append(rowElement);
+});
 
-keys.forEach(key => {
-    const buttonElement = document.createElement('button')
-    buttonElement.textContent = key;
-    buttonElement.setAttribute('id', key);
-    buttonElement.addEventListener('click', () => handleClick(key));
-    keyboard.append(buttonElement)
-})
+keys.forEach((key) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = key;
+  buttonElement.setAttribute("id", key);
+  buttonElement.addEventListener("click", () => handleClick(key));
+  keyboard.append(buttonElement);
+});
 
 const handleClick = (letter) => {
-    console.log('clicked', letter)
-    if (letter === '<<') {
-        deleteLetter()
-        return
-    }
-    if (letter === 'ENTER') {
-        checkRow()
-        return
-    }
-    addLetter(letter)
-}
+  console.log("clicked", letter);
+  if (letter === "<<") {
+    deleteLetter();
+    return;
+  }
+  if (letter === "ENTER") {
+    checkRow();
+    return;
+  }
+  addLetter(letter);
+};
 
 const addLetter = (letter) => {
-    if (currentTile < 5 && currentRow < 6) {
-        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
-    tile.textContent = letter
-    guessRows[currentRow][currentTile] = letter 
-    tile.setAttribute('data', letter)
-    currentTile++
-    console.log('guessRows', guessRows)
-    }
-}
+  if (currentTile < 5 && currentRow < 6) {
+    const tile = document.getElementById(
+      "guessRow-" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = letter;
+    guessRows[currentRow][currentTile] = letter;
+    tile.setAttribute("data", letter);
+    currentTile++;
+    console.log("guessRows", guessRows);
+  }
+};
 
 const deleteLetter = () => {
-    if (currentTile > 0) {
-    currentTile--
-    const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
-    tile.textContent = ''
-    guessRows[currentRow][currentTile] = ''
-    tile.setAttribute('data', '')
-    }
-}    
+  if (currentTile > 0) {
+    currentTile--;
+    const tile = document.getElementById(
+      "guessRow-" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = "";
+    guessRows[currentRow][currentTile] = "";
+    tile.setAttribute("data", "");
+  }
+};
 
 const checkRow = () => {
-    const guess = guessRows[currentRow].join('')
+  const guess = guessRows[currentRow].join("");
 
-    if (currentTile > 4) {
-        console.log('guess is ' + guess, 'boodle is ' + boodle)
-        if (boodle == guess) {
-            showMessage('Spooktacular!')
-            isGameOver = true
-            return
-        } else {
-            if (currentRow >= 5) {
-                isGameOver = true
-                showMessage('Game Over')
-                return
-            }
-            if (currentRow < 5) {
-                currentRow++
-                currentTile = 0
-            }
-        }
+  if (currentTile > 4) {
+    console.log("guess is " + guess, "boodle is " + boodle);
+    if (boodle == guess) {
+      showMessage("Spooktacular!");
+      isGameOver = true;
+      return;
+    } else {
+      if (currentRow >= 5) {
+        isGameOver = true;
+        showMessage("Game Over");
+        return;
+      }
+      if (currentRow < 5) {
+        currentRow++;
+        currentTile = 0;
+      }
     }
-}
+  }
+};
 
 const showMessage = (message) => {
-    const messageElement = document.createElement('p')
-    messageElement.textContent = message
-    messageDisplay.append(messageElement)
-    setTimeout(() => messageDisplay.removeChild(messageElement), 2000)
-}
+  const messageElement = document.createElement("p");
+  messageElement.textContent = message;
+  messageDisplay.append(messageElement);
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
+};
+
+const flipTile = () => {
+  const rowTiles = document.querySelector("guessRow-" + currentRow).childNodes;
+  rowTiles.forEach((tile, index) => {
+    const dataLetter = tile.getAttribute("data");
+
+    if (dataLetter == boodle[index]) {
+      tile.classList.add("green-overlay");
+    } else if (boodle.includes(dataLetter)) {
+      tile.classList.add("yellow-overlay");
+    } else {
+      tile.classList.add("grey-overlay");
+    }
+  });
+};
